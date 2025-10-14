7bb0edf245d02862ddedc7d4a9bc6828cbaea029
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-10-15 00:13:07 +0100

    (bids): created the POST endpoint for bids

--- src/bids/bids.controller.ts
+++ src/bids/bids.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { BidsService } from './bids.service';
+import { CreateBidDto } from './dto/create-bid.dto';
+import { Roles } from '../common/decorators/get-user.decorator';
+import { Role } from '../common/enums/role.enum';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Controller('bids')
-export class BidsController {}
+@UseGuards(JwtAuthGuard, RolesGuard) // ✅ Protege todas as rotas do controller
+export class BidsController {
+  constructor(private readonly bidsService: BidsService) {}
+
+  @Post()
+  @Roles(Role.BUYER)
+  async create(@Req() req, @Body() dto: CreateBidDto) {
+    console.log('Usuário autenticado:', req.user); // 👈 debug
+    return this.bidsService.createBid(dto, req.user.userId);
+  }
+
+  @Get('auction/:auctionId')
+  async findAllByAuction(@Param('auctionId') auctionId: string) {
+    return this.bidsService.findAllByAuction(auctionId);
+  }
+
+  @Get('user/:userId')
+  async findAllByUser(@Param('userId') userId: string) {
+    return this.bidsService.findAllByUser(userId);
+  }
+}
